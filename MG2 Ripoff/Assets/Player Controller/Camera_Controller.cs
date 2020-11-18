dd99a4e944ff30a888b6e5b6b42a7e73368eac77
chrisedwou <cedwards19@mail.wou.edu>
2020-11-17 17:55:05 -0800

    Moved mg2_ripoff game to repo

---
+++ MG2 Ripoff/Assets/Player Controller/Camera_Controller.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Camera_Controller : Player_Controller{
+    public GameObject followTarget;
+    private Vector3 targetPosition;
+    public float peakDistance;
+    public float moveSpeed;
+    private bool peaking;
+
+    private float peakX;
+    private float peakY;
+   
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       /** peaking = false;
+        if (peaking != true)
+        {
+            targetPosition = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+        usePeak();*/
+    }
+
+    bool usePeak()
+    {
+        peakAt(ref lastMove);
+        if (Input.GetAxisRaw("Fire2") > 0.5f)
+        {
+            transform.Translate(new Vector3());
+            peaking = true;
+            return true;
+        }
+        if (Input.GetAxisRaw("Fire2") < 0.5f)
+            return false;
+        return false;
+    }
+
+    void peakAt(ref Vector2 newVec)
+    {
+        peakX = newVec.x;
+        peakY = newVec.y;
+        
+    }
+}
---
+++ MG2 Ripoff/Assets/Player Controller/Player_Controller.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour
+{
+    private Animator anim;
+    private bool playerMoving;
+    private bool pMovingHor;
+    private bool pMovingVert;
+    private bool attacking;
+    private float attackTimeCounter;
+    private float speed = 0;
+
+    public Vector2 lastMove;
+    public float maxSpeed;
+    public float acceleration;
+    public float attackTime;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("Move X", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("Move Y", Input.GetAxisRaw("Vertical"));
+        anim.SetFloat("LastMoveY", lastMove.y);
+        
+
+        attacking = attack();
+
+        if (pMovingHor == false)
+            verticalMovement();
+
+        if (pMovingVert == false)
+            horizontalMovement();
+
+        if (pMovingHor == false && pMovingVert == false)
+        {
+            speed = 0;
+            playerMoving = false;
+        }
+        else
+            playerMoving = true;
+    }
+
+    bool attack()
+    {
+        if (Input.GetAxisRaw("Fire1") > 0.5f)
+        {
+            speed = speed * 0.25f;
+            return true;
+        }
+        if (Input.GetAxisRaw("Fire1") < 0.5f)
+        {
+            return false;
+        }
+        return false;
+    }
+
+    bool verticalMovement()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            if (speed < maxSpeed)
+            {
+                transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * (speed = speed + acceleration) * Time.deltaTime, 0f));
+            }
+            else
+            {
+                transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * maxSpeed * Time.deltaTime, 0f));
+            }
+            pMovingVert = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+        else
+            pMovingVert = false;
+
+        return pMovingVert;
+    }
+
+    bool horizontalMovement() {
+
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (speed < maxSpeed)
+            {
+                transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * (speed = speed + acceleration) * Time.deltaTime, 0, 0));
+            }
+            else
+            {
+                transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * maxSpeed * Time.deltaTime, 0, 0));
+            }
+            pMovingHor = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+        else
+            pMovingHor = false;
+
+        return pMovingHor;
+
+    }  
+}
+
+ 
